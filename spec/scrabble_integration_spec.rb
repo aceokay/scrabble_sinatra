07449d5fdcecc1fac8eead8512093e524ad090cd
Austin Kincaid & Lisa Folliard <student@epicodus.com>
2015-08-05 12:02:11 -0700

    build integration spec to test the processing of user input to a score

---
+++ spec/scrabble_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the scrabble score path', {:type => :feature}) do
+  it('processes the user entry of one word and returns the scrabble score') do
+    visit('/')
+    fill_in('score', :with => 'Expeditious')
+    click_button('Check it!')
+    expect(page).to have_content(21)
+  end
+end
